4ac6e995cb87997ca1448595d45a6dc6f3845cbc
SaifeddineBoudokhane <113269052+SaifeddineBoudokhane@users.noreply.github.com>
2024-02-16 21:10:17 +0100

    add a toggle for read and a Remove book button

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function Book(title="title",author="author",nbrPages=0,status=false){
     this.author=author;
     this.nbrPages=nbrPages;
     this.status=status;
+    this.index=null;
 }
 //functions
     //function that creates a book from dialog input
@@ -26,33 +27,69 @@ function createBook(){
     //function that adds book to library
 function addBookToLibrary(newBook){
     myLibrary.push(newBook);
+    newBook.index=myLibrary.indexOf(newBook);
     displayBook(newBook);
 }
 
     //function that displays a book on the page
 function displayBook(book){
+
+    //div.book
     const div=document.createElement("div");
     div.classList.add("book");
+    div.id=book.index;
+    //p.title
     const title=document.createElement("p");
     title.classList.add("title");
     title.textContent='"'+book.title+'"';
+
+    //p.author
     const author=document.createElement("p")
     author.classList.add("author");
     author.textContent=book.author;
+
+    //p.pages
     const pages=document.createElement("p");
     pages.classList.add("pages");
     pages.textContent=book.nbrPages+" Pages"
-    const status=document.createElement("p");
+
+    //button.status
+    const status=document.createElement("button");
     status.classList.add("status");
+    status.value=book.index;
     if(book.status==true){
         status.textContent="Read";
+        status.classList.add("read");
     }else{
         status.textContent="Not Read";
     }
+    status.addEventListener("click",()=>{
+        if(status.textContent=="Read"){
+            status.classList.remove('read');
+            status.textContent="Not Read"
+            myLibrary[Number(status.value)].status=false;
+        }else{
+            status.classList.add('read');
+            status.textContent="Read";
+            myLibrary[Number(status.value)].status=true;
+        }
+    })
+    //button.remove
+    const remove=document.createElement("button")
+    remove.classList.add("remove");
+    remove.textContent="Remove";
+    remove.value=book.index;
+    remove.addEventListener("click",()=>{
+        myLibrary[Number(remove.value)]=null;
+        const divRemove=document.getElementById(`${remove.value}`);
+        divRemove.remove();
+    })
+    //add to DOM
     div.appendChild(title);
     div.appendChild(author);
     div.appendChild(pages);
     div.appendChild(status);
+    div.appendChild(remove);
     bookList.insertAdjacentElement("afterbegin",div)
 }
 
